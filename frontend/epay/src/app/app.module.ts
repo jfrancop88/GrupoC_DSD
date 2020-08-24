7ff96b3aad2a7ea239753087328ee1ef773ed386
jfranco88 <jfranco88@gmail.com>
2020-08-23 23:23:51 -0500

    Checkout implemented

--- frontend/epay/src/app/app.module.ts
+++ frontend/epay/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComercioComponent } from './comercio/comercio.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComercioComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
